0beeb4a6e7e771bdc29dea889b9ff9011b88a8ba
Joe <josehinojosa8@gmail.com>
2017-07-07 11:31:59 -0400

    revised

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ app.get("/users", function(req,res)  {
   res.render('users',  {users: data.users})
 });
 
-app.get('users/:id'), function(req, res){
+app.get('/users/:id', function(req, res){
   let id =req.params.id;
   let user = data.users[id];
-  res.render('user, user')
-})
+  res.render('user', user)
+});
 
 
 app.listen(3000, function ()  {
